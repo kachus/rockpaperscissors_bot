a6321e036d27ce3a861819044bc3bbc1bd415e32
kachus <ka4usova.e@gmail.com>
2023-01-30 14:30:25 +0400

    ru keyboard added

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -10,5 +10,15 @@ yes_no_kb: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
 button_yes: KeyboardButton = KeyboardButton(LEXICON_RU['yes_button'])
 button_no: KeyboardButton = KeyboardButton(LEXICON_RU['no_button'])
 
+#buttons are near each other
+yes_no_kb.add(button_yes, button_no)
 
+#creating a game keyboard with rock, paper, scissors buttons
+game_kb: ReplyKeyboardMarkup = ReplyKeyboardMarkup(resize_keyboard=True)
 
+button_1: KeyboardButton = KeyboardButton(LEXICON_RU['rock'])
+button_2: KeyboardButton = KeyboardButton(LEXICON_RU['scissors'])
+button_3: KeyboardButton = KeyboardButton(LEXICON_RU['paper'])
+
+#buttons are under each others
+game_kb.add(button_1).add(button_2).add(button_3)
